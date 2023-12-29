d979234d2e6bb1851089e0c934781a60c313f689
Aditya-Raj-009 <adityasinghrajput2107200@gmail.com>
2023-12-29 14:11:17 +0530

    Firebase integration

--- app/src/main/java/com/avi/adminwall/MainActivity.kt
+++ app/src/main/java/com/avi/adminwall/MainActivity.kt
@@ -1,16 +1,40 @@
 package com.avi.adminwall
+
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.avi.adminwall.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.app
+import com.google.firebase.ktx.initialize
 
 class MainActivity : AppCompatActivity() {
     private lateinit var chooseCategoryBinding: ActivityMainBinding
+
+    private lateinit var firebaseInstance: FirebaseDatabase
+    private lateinit var databaseReference: DatabaseReference
+
+    private lateinit var division: String
+    private lateinit var imageUri:String
+
+    private var options = FirebaseOptions.Builder()
+        .setProjectId("infinity-walls")
+        .setApplicationId("1:355548059361:android:7bf635a2d582f5cb32c9a0")
+        .setApiKey("AIzaSyBdNm-G_5Xh3oZ-Q9ulUPjYD5sqBkB01zU")
+        .setDatabaseUrl("https://infinity-walls-default-rtdb.firebaseio.com/")
+        .build()
 
     // Use ActivityResultContracts to handle image selection
     private val getContent =
@@ -20,6 +44,7 @@ class MainActivity : AppCompatActivity() {
                 val selectedImageUri = result.data?.data
                 // Do something with the selected image URI, e.g., set it to the ImageView
                 chooseCategoryBinding.userProfileImage.setImageURI(selectedImageUri)
+                imageUri = selectedImageUri.toString()
                 Toast.makeText(this, "Image selected", Toast.LENGTH_SHORT).show()
             }
         }
@@ -29,6 +54,12 @@ class MainActivity : AppCompatActivity() {
         chooseCategoryBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = chooseCategoryBinding.root
         setContentView(view)
+        // Initialize Firebase
+        Firebase.initialize(this, options,"secondary")
+        val second = Firebase.app("secondary")
+
+        firebaseInstance = FirebaseDatabase.getInstance(second)
+        databaseReference = firebaseInstance.getReference("Category")
 
         // Set up the category list dropdown
         val categoryList = arrayOf(
@@ -42,6 +73,32 @@ class MainActivity : AppCompatActivity() {
         // Set up onClickListener for the ImageView to choose an image
         chooseCategoryBinding.userProfileImage.setOnClickListener {
             pickImageFromGallery()
+        }
+
+        chooseCategoryBinding.buttonPush.setOnClickListener {
+            division = autoCompleteTextView.text.toString()
+            if (division == "Choose Category") {
+                Toast.makeText(applicationContext, "Select category", Toast.LENGTH_SHORT).show()
+            }
+            else if(imageUri.isEmpty())
+            {
+                Toast.makeText(applicationContext, "Select Image", Toast.LENGTH_SHORT).show()
+            }
+            else if(chooseCategoryBinding.EditName.text.toString().isEmpty())
+            {
+                Toast.makeText(applicationContext,"Set a name",Toast.LENGTH_SHORT).show()
+            }
+            else {
+                databaseReference.child(division).
+                child(chooseCategoryBinding.EditName.text.toString())
+                    .push().setValue(imageUri)
+                    .addOnSuccessListener {
+                        Log.d("Data",imageUri+" added successfully")
+                    }
+
+                Toast.makeText(applicationContext,"Select category",Toast.LENGTH_SHORT).show()
+
+            }
         }
     }
 
